abcef6b85a1e679811ae7560bcbeec6d71b03afb
AndrewRamos <103224394+AndrewRamosRJ@users.noreply.github.com>
2022-08-15 10:48:44 -0300

    Update imc.js

--- js/imc.js
+++ js/imc.js
@@ -5,99 +5,92 @@
     
 
 
-    function go(currentStep,nextStep)
-    {
+    function go(currentStep,nextStep){
         let dNone, dBlock;
-        if(currentStep == 1)
-        {
+        
+        if(currentStep == 1) {
+            
             dNone = firstDiv;
         }
         else if(currentStep == 2)
         {
             dNone = secondDiv;
         }
-        else
-        {
+        else {
             dNone = finalDiv;
         }
         
         dNone.style.display = 'none';
 
-        if(nextStep == 1)
-        {
+        if(nextStep == 1) {
+            
             dBlock = firstDiv;
-        }
-        else if(nextStep == 2)
-        {
+            
+        } else if(nextStep == 2) {
+            
             dBlock = secondDiv;
         }
-        else
-        {
+        else {
             dBlock = finalDiv;
         }
+        
         dBlock.style.display = 'block';
     }
 
-    function validate()
-    {
+    function validate() {
+        
         const peso   = document.getElementById('peso');
         const altura = document.getElementById('altura');
         
         peso.style.border   = 'none';
         altura.style.border = 'none';
-        if(!peso.value || !altura.value)
-        {
-            if(!peso.value && !altura.value)
-            {
+        if(!peso.value || !altura.value) {
+            
+            if(!peso.value && !altura.value) {
+                
                 peso.style.border = '1px solid red';
                 altura.style.border = '1px solid red';
                 alert('Você deve preencher seu peso e a sua altura')
-            }
-            else if(!peso.value)
-            {
+                
+            } else if(!peso.value) {
+                
                 peso.style.border = '1px solid red';
                 alert('Você deve preencher seu peso e a sua altura')
-            }
-            else
-            {
+            } else {
                 altura.style.border = '1px solid red';
                 alert('Você deve preencher seu peso e a sua altura')
             }
-        }
-        else
-        {           
+        } else {           
             const imc = (peso.value / (altura.value * altura.value)).toFixed(2);
             let classificacao = '';
             const result = document.getElementById('resultado');
             const nome = document.getElementById('nome').value;
-            if(imc < 18.5)
-            {
+            
+            if(imc < 18.5) {
                 classificacao = 'abaixo do peso.';
                 result.style.color = 'yellow';
                 result.innerHTML = result.innerHTML = `${nome} seu IMC é ${imc} e você está ${classificacao}`;
-            }
-            else if(imc >= 18.5 && imc < 25)
-            {
+                
+            } else if(imc >= 18.5 && imc < 25) {
+                
                 classificacao = 'com o peso ideal. Parabéns!!!';
                 result.innerHTML = result.innerHTML = `${nome} seu IMC é ${imc} e você está ${classificacao}`;
-            }
-            else if(imc >= 25 && imc < 30)
-            {
+                
+            } else if(imc >= 25 && imc < 30) {
                 classificacao = 'levemente acima do peso.';
                 result.innerHTML = result.innerHTML = `${nome} seu IMC é ${imc} e você está ${classificacao}`;
-            }
-            else if(imc >= 30 && imc < 35)
-            {
+                
+            } else if(imc >= 30 && imc < 35) {
+                
                 classificacao = 'com obesidade grau I Atenção: Procure um especialista';
                 result.innerHTML = result.innerHTML = `${nome} seu IMC é ${imc} e você está ${classificacao}`;
-            }
-            else if (imc >= 35 && imc < 40) 
-            {
+                
+            } else if (imc >= 35 && imc < 40) {
+                
                 classificacao = 'com obesidade grau II Atenção: Procure um especialista';
                 result.innerHTML = result.innerHTML = `${nome} seu IMC é ${imc} e você está ${classificacao}`;
-            }
-            else
-            {
+            } else {
+                
                 classificacao = 'com obesidade grau III Muita atenção: Procure um especialista';
                 result.innerHTML = `${nome} seu IMC é ${imc} e você está ${classificacao}`;
                 
@@ -125,9 +118,6 @@
               });
             
             go(2,3);    
-
         }
-              
-    
     }
 
